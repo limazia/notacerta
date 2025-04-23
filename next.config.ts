289f712bd4db971a3684d767803f139f8de48451
Acacio de Lima <limadeacacio@gmail.com>
2025-04-23 15:37:06 -0300

    Add environment variables configuration for OpenAI and Supabase

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+    SUPABASE_AUTH: process.env.SUPABASE_AUTH,
+  },
 };
 
 export default nextConfig;
