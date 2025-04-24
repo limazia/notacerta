a7227d951c85b9ddda2221b08281a6d9dc6cf92f
Acacio de Lima <limadeacacio@gmail.com>
2025-04-24 14:07:09 -0300

    Refactor API delay condition in axios configuration

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+const API_DELAY = false;
+
 export const api = axios.create({
   baseURL: "/api",
 });
 
-if (true) {
+if (API_DELAY) {
   api.interceptors.request.use(async (config) => {
     await new Promise((resolve) =>
       setTimeout(resolve, Math.round(Math.random() * 4000))
